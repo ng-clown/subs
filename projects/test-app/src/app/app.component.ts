48d9039b0116b88e36833458f0258af0703aa4b0
Darius Bualan <mfpjayb@gmail.com>
2022-09-13 23:30:25 +0800

    updated subs implementation

--- projects/test-app/src/app/app.component.ts
+++ projects/test-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subs } from 'projects/subs/src/public-api';
 import { User } from './models/user.model';
 
 @Component({
@@ -6,17 +7,27 @@ import { User } from './models/user.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'test-app';
+  counter = 0;
 
-  constructor(private user: User) {
-    this.user.observable.subscribe(value => {
+  constructor(private user: User, private subs: Subs) {
+    this.subs.add(this.user.observable);
+  }
+
+  ngOnInit(): void {
+    this.user.observable.subscribe((value) => {
+      this.counter++;
       console.log(value);
+      if (this.counter >= 10) {
+        this.subs.unsubscribeAll();
+      }
     });
-    this.user.firstName = 'hi';
-    this.user.unsubscribeAll();
-    this.user.lastName = 'hi';
+
+    setInterval(() => {
+      this.user.firstName = new Date().getTime().toString();
+    }, 1000);
   }
 
 }
